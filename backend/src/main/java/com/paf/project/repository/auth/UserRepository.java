d1d09f143d0ac0afd4e275ead3a9f0c23bb74404
Rasanjana-61 <pramothrasanjana@gmail.com>
2026-04-20 23:15:05 +0530

    feat: change UserRepository to extend MongoRepository with String ID type

--- backend/src/main/java/com/paf/project/repository/auth/UserRepository.java
+++ backend/src/main/java/com/paf/project/repository/auth/UserRepository.java
@@ -3,13 +3,13 @@ package com.paf.project.repository.auth;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.paf.project.model.auth.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, String> {
 
     Optional<User> findByEmail(String email);
 
