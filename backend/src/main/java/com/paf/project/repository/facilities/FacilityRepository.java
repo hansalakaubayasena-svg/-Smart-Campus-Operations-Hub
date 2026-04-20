acde1b06b2f8eab50cc3dc24cc332d94430164f8
Rasanjana-61 <pramothrasanjana@gmail.com>
2026-04-20 23:16:02 +0530

    feat: change FacilityRepository to extend MongoRepository with String ID type

--- backend/src/main/java/com/paf/project/repository/facilities/FacilityRepository.java
+++ backend/src/main/java/com/paf/project/repository/facilities/FacilityRepository.java
@@ -1,9 +1,9 @@
 package com.paf.project.repository.facilities;
 
 import com.paf.project.model.facilities.Facility;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FacilityRepository extends JpaRepository<Facility, Long> {
+public interface FacilityRepository extends MongoRepository<Facility, String> {
 }
